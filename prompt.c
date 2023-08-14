8382ae3cc32428dc3d434e19299a798a67a7113a
masichaivy <lyndamasicha@gmail.com>
2023-08-14 09:28:35 -0700

    create a prompt for the shell

---
+++ prompt.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * prompt - displays prompt
+ * @av: arg vector
+ * @env: environment variable
+ *
+ * Return: void
+ */
+
+void prompt(char **av, char **env)
+{
+	char *str = NULL;
+	int n, i;
+	ssize_t len;
+	char *cmd[];
+
+	n = 0;
+	cmd = {NULL, NULL};
+	while (1)
+	{
+		printf("savi->$ ");
+
+		len = getline(&str, &n, stdin);
+
+		if (len == -1)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] == '\n')
+				str[i] = 0;
+		}
+	}
+}
