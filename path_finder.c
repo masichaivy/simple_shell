00a0eb66637dc846e782dfcee2876bdf1c596eff
qurriahSam <mugirethe1@gmail.com>
2023-08-23 09:57:17 -0700

    feat: handle if user enters full path

--- path_finder.c
+++ path_finder.c
@@ -4,35 +4,45 @@
 
 int path_finder(const char *cmd, char **user_cmd)
 {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *dir = strtok(path_copy, ":");
-    
-    if (path == NULL)
-    {
-        printf("Error: PATH variable not found\n");
-        return 0;
-    }
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *dir = strtok(path_copy, ":");
 
+	if (path == NULL)
+	{
+		printf("Error: PATH variable not found\n");
+		return (0);
+	}
 
-    while (dir != NULL)
-    {
-        char full_path[MAX_PATH_LEN];
-        str_cpy(full_path, dir);
-        str_cat(full_path, "/");
-        str_cat(full_path, cmd);
+	if (cmd[0] == '/')
+	{
+		if (access(cmd, X_OK) == 0)
+		{
+			user_cmd[0] = str_dup(cmd);
+			return (1);
+		}
+		return (0);
+	}
 
-        if (access(full_path, X_OK) == 0)
-        {
-            user_cmd[0] = str_dup(full_path);
-            free(path_copy);
-            return 1;
-        }
+	while (dir != NULL)
+	{
+		char full_path[MAX_PATH_LEN];
 
-        dir = strtok(NULL, ":");
-    }
+		str_cpy(full_path, dir);
+		str_cat(full_path, "/");
+		str_cat(full_path, cmd);
 
-    free(path_copy);
-    return 0;
+		if (access(full_path, X_OK) == 0)
+		{
+			user_cmd[0] = str_dup(full_path);
+			free(path_copy);
+			return (1);
+		}
+
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (0);
 }
 
