cf4e42f79bcbe141240004f1c80c40a7c1cc2a1f
qurriahSam <mugirethe1@gmail.com>
2023-08-24 10:07:35 -0700

    style: move prompt functions to main

--- main.c
+++ main.c
@@ -1,17 +1,41 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @env: environment vector
+ * main - main function.
+ * @ac: arguments number.
+ * @av: array of arguments.
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0.
  */
-
-int main(int ac, char **env)
+int main(int ac, char **av)
 {
-	if (ac == 1)
-		prompt(env);
+	char *buffer = NULL, *buffer_copy = NULL;
+	size_t buffer_size = 0;
+	ssize_t n_char;
 
+	(void)av;
+	if (ac == 1)
+		while (1)
+		{
+			write(1, "savi=>$ ", 2);
+			n_char = _getline(&buffer, &buffer_size, stdin);
+			if (n_char == -1)
+			{
+				printf("\n");
+				return (-1);
+			}
+			buffer_copy = malloc(sizeof(char) * n_char);
+			if (buffer_copy == NULL)
+			{
+				perror("buffer_copy: malloc error");
+				return (-1);
+			}
+			strcpy(buffer_copy, buffer);
+			handle_args(buffer, buffer_copy);
+
+			free(buffer_copy);
+		}
+	free(buffer);
 	return (0);
 }
+
